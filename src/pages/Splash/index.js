92716587e97ea2b13ad1fa79f4b0cec995386bde
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-06-25 19:41:06 +0700

    add splash component and call in Router with initialRouteName

---
+++ src/pages/Splash/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Splash = () => {
+  return (
+    <View>
+      <Text>Ini Splash Scren</Text>
+    </View>
+  )
+}
+
+export default Splash
+
+const styles = StyleSheet.create({})
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../pages/Home';
+import Splash from '../pages/Splash';
 
 const Stack = createStackNavigator();
 
 const Router = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName="Splash">
       <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
+      <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}} />
     </Stack.Navigator>
   )
 }
