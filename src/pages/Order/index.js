5c69da635a60885e817318c3feb6a01a6ef60872
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-06-25 19:53:39 +0700

    add bottom navigation and other component for routing pages

---
+++ src/pages/Order/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Order = () => {
+  return (
+    <View>
+      <Text>Ini Order</Text>
+    </View>
+  )
+}
+
+export default Order
+
+const styles = StyleSheet.create({})
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../pages/Home';
 import Splash from '../pages/Splash';
+import Order from '../pages/Order';
+import Account from '../pages/Account';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Order" component={Order} />
+      <Tab.Screen name="Account" component={Account} />
+    </Tab.Navigator>
+  )
+}
 
 const Router = () => {
   return (
-    <Stack.Navigator initialRouteName="Splash">
-      <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
+    <Stack.Navigator initialRouteName="MainApp">
       <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}} />
+      <Stack.Screen name="MainApp" component={MainApp} options={{headerShown: false}} />
     </Stack.Navigator>
   )
 }
