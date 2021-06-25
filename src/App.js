25281c082cd89043586c979ab9b0054b2617af98
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-06-25 19:35:03 +0700

    refactoring stack navigator into Router component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-const Stack = createStackNavigator();
+import Router from './router';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <Router />
     </NavigationContainer>
   )
 }
