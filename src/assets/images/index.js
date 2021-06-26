a0a8a92cd3d588562caa8d05642815648da522aa
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-06-26 14:53:49 +0700

    add header for styling home component and add new font into android project

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,7 +1,9 @@
 import SplashBackground from './splashBackground.png'
 import SplashLogo from './splashLogo.png'
+import ImageHeader from './header.png'
 
 export {
   SplashBackground,
-  SplashLogo
+  SplashLogo,
+  ImageHeader
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,50 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ImageBackground, Dimensions, Image } from 'react-native'
+import { ImageHeader, SplashLogo } from '../../assets/images'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Ini Home</Text>
+    <View style={styles.container}>
+      <ImageBackground source={ImageHeader} style={styles.header}>
+        <Image source={SplashLogo} style={styles.logo} />
+        <View style={styles.text}>
+          <Text style={styles.welcome}>Welcome,</Text>
+          <Text style={styles.username}>muhammadikbal21_</Text>
+        </View>
+      </ImageBackground>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+// untuk mengukur lebar atau tinggi halaman dari berbagai perangkat agar halaman menjadi responsif
+const windowWidth = Dimensions.get('window').width
+const windowHeight = Dimensions.get('window').height
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  header: {
+    width: windowWidth,
+    height: windowHeight * 0.3,
+    paddingHorizontal: 30,
+    paddingTop: 10
+  },
+  logo: {
+    width: windowWidth * 0.25,
+    height: windowHeight * 0.06
+  },
+  text: {
+    marginTop: windowHeight * 0.025
+  },
+  welcome: {
+    fontSize: 24,
+    fontFamily: 'TitilliumWeb-Regular'
+  },
+  username: {
+    fontSize: 20,
+    fontFamily: 'TitilliumWeb-Bold'
+  }
+})
